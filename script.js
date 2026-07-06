a6125d8852c8601395cb0dbb183ac9c9badd24e9
10315545-commits <10315545@clases.edu.sv>
2026-07-06 10:39:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+const eventos = [
+    {
+        nombre: "Free Coins",
+        descripcion: "Aparecen monedas gratis por tiempo limitado."
+    },
+    {
+        nombre: "Asteroid",
+        descripcion: "Caen asteroides por el mapa."
+    },
+    {
+        nombre: "Free Gems",
+        descripcion: "Los jugadores reciben gemas gratis."
+    },
+    {
+        nombre: "Trollge Event",
+        descripcion: "Aparecen Trollges especiales con recompensas."
+    }
+];
